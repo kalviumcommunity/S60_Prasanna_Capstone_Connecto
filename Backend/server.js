b00aff99aa8360cffe7930ac9794f5bc59ced0d6
Prasanna V <you@example.com>
2024-07-31 11:19:18 +0530

    connected db with the code and used it whereever it is need to inside get and post operatio ns

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const MONGO_URI = process.env.MONGO_URI;
 
 // express assignd to app variable
 const app = express();
@@ -12,6 +13,15 @@ const app = express();
 // using cors and body parser middlewares
 app.use(cors());
 app.use(bodyParser.json());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log('connected to DB');
+  } catch (error) {
+    console.log(error, 'error in connecting');
+  }
+};
 
 // Mongodb-mongoose schema that is used to create a entity in the mainpage.
 const MainSchema = new mongoose.Schema({
@@ -25,24 +35,28 @@ const MainSchema = new mongoose.Schema({
   ExpectedSalary: String,
 });
 
+const ModelDb = mongoose.model('db', MainSchema);
+
 // defining a "GET" route called main
 app.get('/main', (req, res) => {
-  then((res) => {
-    res.send('get route');
-  }).catch((error) => {
-    res.status(500).send({ error: error.toString() });
-  });
+  ModelDb.find({}) //find function is used to fetch details from the database.
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 // defining a "POST" route
 app.post('/main', async (req, res) => {
-  then((res) => {
-    return res.send(req.body);
-  }).catch((error) => {
-    console.log(error);
-  });
+  console.log(req.body, '____');
+  await ModelDb.create(req.body);
+  return res.send(req.body);
 });
 
-app.listen(5335, async () => {
-  console.log('Port listening to 5335 port');
+const PORT = 5335;
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Port listening to ${PORT} port`);
 });
