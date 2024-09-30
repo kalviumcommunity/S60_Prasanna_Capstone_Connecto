53db65798187352e66cea04869d34030737cf8a2
Prasanna V <you@example.com>
2024-09-30 10:39:49 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -90,6 +90,138 @@ app.get('/', (res, req) => {
 
 //routes to login aand signup
 
+app.post('/signup', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (user) {
+    res.status(400).send({ message: 'get out' });
+  } else {
+    const { error, value } = userJoiSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    if (req.body.email !== req.body.confirmemail) {
+      return res.status(400).json({ message: 'Emails do not match' });
+    }
+
+    if (req.body.password !== req.body.confirmpassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    try {
+      const salt = await bcrypt.genSalt(10);
+
+      const hashedPassword = await bcrypt.hash(value.password, salt);
+
+      const newUser = await User.create({ ...value, password: hashedPassword });
+
+      return res.status(201).send({ message: 'ok' }); // Status 201 for successful creation
+    } catch (error) {
+      console.log('error', error);
+
+      return res.status(500).send({ message: 'Server error' });
+    }
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    if (user && bcrypt.compare(req.body.password, user.password)) {
+      res.json({ message: 'Login successful' });
+    } else {
+      res.send({ message: 'Invalid credentials or user not existed' });
+    }
+  } catch (error) {
+    console.log(error);
+
+    res.send('Error in login');
+  }
+});
+
+app.post('/google-login', async (req, res) => {
+  const { token } = req.body;
+
+  console.log('Received token:', token);
+
+  if (!token) {
+    return res.status(400).json({ message: 'No token provided' });
+  }
+
+  const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+
+    const { email, name, sub: googleId } = payload;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      const salt = await bcrypt.genSalt(10);
+
+      const hashedPassword = await bcrypt.hash(
+        googleId + process.env.PASSWORD_SALT,
+
+        salt
+      );
+
+      user = new User({
+        name,
+
+        email,
+
+        password: hashedPassword,
+
+        googleId,
+      });
+
+      await user.save();
+    }
+
+    res.json({
+      message: 'Login successful',
+
+      userId: user._id,
+
+      name: user.name,
+
+      email: user.email,
+    });
+  } catch (error) {
+    console.error('Error during Google login:', error);
+
+    if (error.message.includes('Token used too late')) {
+      res
+
+        .status(400)
+
+        .json({ message: 'Token expired. Please try logging in again.' });
+    } else if (error.message.includes('Invalid token')) {
+      res
+
+        .status(400)
+
+        .json({ message: 'Invalid token. Please try logging in again.' });
+    } else {
+      res.status(500).json({ message: 'Server error during authentication' });
+    }
+  }
+});
+
 app.post('/main', async (req, res) => {
   console.log(
     req.body,
