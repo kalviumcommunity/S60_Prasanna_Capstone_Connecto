875f11b80290ebb2b827511843ee0a4b02c1b1f5
Prasanna V <you@example.com>
2024-09-30 10:34:59 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -87,32 +87,8 @@ app.get('/', (res, req) => {
     console.log('some error in backend');
   }
 });
-// app.post('/signup', async (req, res) => {
-//   const user = await User.findOne({ email: req.body.email });
-//   if (user) {
-//     res.send({ message: 'get out' });
-//   } else {
-//     const { error, value } = userJoiSchema.validate(req.body);
-//     if (error) {
-//       return res.status(400).json({ message: error.details[0].message });
-//     }
-//     if (req.body.email !== req.body.confirmemail) {
-//       return res.status(400).json({ message: 'Emails do not match' });
-//     }
-//     if (req.body.password !== req.body.confirmpassword) {
-//       return res.status(400).json({ message: 'Passwords do not match' });
-//     }
-//     try {
-//       const salt = await bcrypt.genSalt(10);
-//       const hashedPassword = await bcrypt.hash(value.password, salt);
-//       const newUser = await User.create({ ...value, password: hashedPassword });
-//       res.send({ message: 'ok' });
-//     } catch (error) {
-//       console.log('error', error);
-//     }
-//   }
-// });
 
+//routes to login aand signup
 
 app.post('/main', async (req, res) => {
   console.log(
